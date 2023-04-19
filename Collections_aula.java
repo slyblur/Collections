fd679f059c347136dc6723ef56d1b7bb52f21861
GYO <kuroigyo@gmail.com>
2023-04-19 19:26:11 -0300

    feito

---
+++ Collections_aula.java
@@ -0,0 +1,24 @@
+package Collection;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Collections_aula {
+
+	public static void main(String[] args) {
+		
+		
+		ArrayList<Double> notas = new ArrayList<>();
+		
+		notas.add(7.0);
+		notas.add(5.0);
+		notas.add(4.0);
+		notas.add(10.0);
+		notas.add(6.0);
+		
+		
+		System.out.println("\n Notas cadastradas: "+notas.toString());
+
+	}
+
+}
